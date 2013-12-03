8a0e85421c4759dfdc05b8ce6b42ee7c4b658004
zinux <zinux@zinux-0S.(none)>
2013-12-03 23:39:32 +0700

    init updatedelete'

---
+++ README.md
@@ -0,0 +1,3 @@
+<h2>SignUP Ch. 8 </h2>
+
+link : https://obscure-atoll-3038.herokuapp.com
---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+
+	#before_save { self.email = email.downcase}
+
+	#exercise 2
+	before_save {email.downcase!}
+	before_create :create_remember_token
+
+	validates :name, presence: true, length: {maximum: 50}
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+	validates(:email, presence: true, format: {with: EMAIL_REGEX},
+					  uniqueness: {case_sensitive: false})
+	has_secure_password
+	validates(:password, length: {minimum: 6})
+	#validates(:password, presence: true, length: {minimum: 6})
+	#validates(:password_confirmation, presence: true)
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+		def create_remember_token
+			self.remember_token = User.encrypt(User.new_remember_token)
+		end
+end
