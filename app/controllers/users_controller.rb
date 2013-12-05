63efa1dc1d35297361db1a5360754f486f7f0e4d
zinux <zinux@zinux-0S.(none)>
2013-12-05 08:39:32 +0700

    end update delete ch. 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  #exercise 6
+  before_action :isnot_loged_in, only: [:new, :create]
+    
   def new
   	@user  = User.new
   end
@@ -20,15 +26,72 @@ class UsersController < ApplicationController
     end 
   end
 
+  def edit
+      #exercise 1
+      if current_user.admin?
+        redirect_to @user, notice: "cannot updating admin"
+      else
+        @user = User.find(params[:id])
+      end
+  end
+
+  def update
+      @user = User.find(params[:id])
+      if(@user.update_attributes(user_params))
+          flash[:success] = 'Profile updated'
+          redirect_to @user
+      else
+          render 'edit'
+      end
+  end
+
+
   def home
   end
 
+  def index
+     @users = User.paginate(page: params[:page])
+  end 
   
+#exercise 9
+  def destroy
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      @user.destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    else
+      redirect_to users_url
+    end
+  end
 
  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+    #exercise 6
+    def isnot_loged_in
+      unless !signed_in? 
+        redirect_to root_url
+      end
     end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,4 @@
+=begin
 FactoryGirl.define do 
 	factory :user do
 		name	"Example User"
@@ -5,4 +6,18 @@ FactoryGirl.define do
 		password "foobar"
 		password_confirmation "foobar"
 	end	
+end
+=end
+FactoryGirl.define do
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+
+  end
 end
